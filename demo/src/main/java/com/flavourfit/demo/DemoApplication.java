772549a5b47ee1d033191e9fc80c45319dc8f2b3
Somya Agarwal <somyaagarwal@tb920.wpa.dal.ca>
2023-06-02 10:43:14 -0300

    springboot project 1

--- demo/src/main/java/com/flavourfit/demo/DemoApplication.java
+++ demo/src/main/java/com/flavourfit/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	System.out.println("Test");
+	//dhdjjd//
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
