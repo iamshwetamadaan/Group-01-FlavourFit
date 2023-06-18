7c52d3d8106deb90d74cfef49f873a415335829e
Dheeraj Bhat <111560019+XPROSYS-Dev@users.noreply.github.com>
2023-06-18 13:52:15 -0300

    Update code and test for checking cicd

--- src/test/java/User/UserDaoTest.java
+++ src/test/java/User/UserDaoTest.java
@@ -1,5 +1,6 @@
 package User;
 
+import com.flavourfit.User.User;
 import com.flavourfit.User.UserDao;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,5 +27,22 @@ public class UserDaoTest {
 
         // Test when user exists
         assertTrue(userDao.userExists("User-1"));
+    }
+
+    @Test
+    public void getUserByIdTest() {
+        // Test when userId is null
+        assertNull(userDao.getUserById(null));
+
+        // Test when userId is empty
+        assertNull(userDao.getUserById(""));
+
+        // Test when user does not exist
+        assertNull(userDao.getUserById("User-999"));
+
+        // Test when user exists
+        User user = userDao.getUserById("User-1");
+        assertNotNull(user);
+        assertEquals("User-1", user.getUserId());
     }
 }
