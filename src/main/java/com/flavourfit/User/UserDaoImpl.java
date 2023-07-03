4d31ba1d63fc9c6cd0228c1bda9e4dafb047ef00
Dheeraj Bhat <dh210086@dal.ca>
2023-07-03 19:24:50 -0300

    Added interface for UserService

--- src/main/java/com/flavourfit/User/UserDaoImpl.java
+++ src/main/java/com/flavourfit/User/UserDaoImpl.java
@@ -4,8 +4,6 @@ import com.flavourfit.DatabaseManager.DatabaseManagerImpl;
 import com.flavourfit.DatabaseManager.IDatabaseManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
