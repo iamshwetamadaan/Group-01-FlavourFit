0c4bf04e070200cbae6a088806f28bf23b6941e2
Somya Agarwal <somyaagarwal@Somyas-MacBook-Pro.local>
2023-07-11 18:04:19 -0300

    feature gaf-11

--- src/main/java/com/flavourfit/User/IUserService.java
+++ src/main/java/com/flavourfit/User/IUserService.java
@@ -6,4 +6,6 @@ public interface IUserService {
     public String fetchAllUsers() throws SQLException;
 
     public int updateUser(UserDto user) throws SQLException;
+
+    UserDto getUserbyID(int user) throws SQLException;
 }
--- src/main/java/com/flavourfit/User/UserController.java
+++ src/main/java/com/flavourfit/User/UserController.java
@@ -1,17 +1,18 @@
 package com.flavourfit.User;
 
-import com.flavourfit.DatabaseManager.DatabaseManagerImpl;
-import com.flavourfit.DatabaseManager.IDatabaseManager;
+import com.flavourfit.ResponsesDTO.PutResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.json.simple.JSONObject;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
@@ -29,47 +30,67 @@ public class UserController {
     }
 
 //    Method to edit user details.
+
     /**
      * Path - /user/update-user
      * Request Params = {
-     *  "userId" : 1,
-     *   "firstName": "John",
-     *   "lastName": "Doe",
-     *   "email": "john@doe.co",
-     *   "contact": "+1234567890",
-     *   "height": 170,
-     *   "weight": 70,
-     *   "targetWeight": 80,
-     *
-     *       "street":"6385 South Street",
-     *       "State":"NS",
-     *       "city":"Halifax",
-     *       "postal_code":"B3H 4j4"
-     *
-     *   "preferences": ["Vegetarian", "Eggs"],
+     * "userId" : 1,
+     * "firstName": "John",
+     * "lastName": "Doe",
+     * "email": "john@doe.co",
+     * "contact": "+1234567890",
+     * "height": 170,
+     * "weight": 70,
+     * "targetWeight": 80,
+     * <p>
+     * "street":"6385 South Street",
+     * "State":"NS",
+     * "city":"Halifax",
+     * "postal_code":"B3H 4j4"
+     * <p>
+     * "preferences": ["Vegetarian", "Eggs"],
      * }
-     *
+     * <p>
      * Response = {
-     *     "success": true,
-     *     "message": "User updated successfully",
-     *     "data": {
-     *     }
+     * "success": true,
+     * "message": "User updated successfully",
+     * "data": {
+     * }
      * }
      */
     @PutMapping("/update-user")
-    public JSONObject editUser(@RequestBody UserDto user) throws SQLException{
+    public JSONObject editUser(@RequestBody UserDto user) throws SQLException {
         int count = userService.updateUser(user);
         JSONObject res = new JSONObject();
-        if(count==0){
-            res.put("success",false);
-            res.put("message","User details invalid");
-            res.put("data",null);
-        }
-        else{
-            res.put("success",true);
-            res.put("message","User details successfully updated");
-            res.put("data",user);
+        if (count == 0) {
+            res.put("success", false);
+            res.put("message", "User details invalid");
+            res.put("data", null);
+        } else {
+            res.put("success", true);
+            res.put("message", "User details successfully updated");
+            res.put("data", user);
         }
         return res;
     }
+
+    @RequestMapping("/getuser-current")
+    public ResponseEntity<PutResponse> getUserByID(@RequestBody Map<Integer, Object> request) {
+        int userID = (int) request.get("userID");
+        try {
+            Map<String, Object> userdata = new HashMap<>();
+            UserDto userDto = this.userService.getUserbyID(userID);
+            if (userDto != null) {
+                userdata.put("user details", userDto);
+                return ResponseEntity.ok().body(new PutResponse(true, "Successfully loaded user details", userdata));
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (SQLException e) {
+            return ResponseEntity.badRequest().body(new PutResponse(false, "Failed to load user details"));
+
+        }
+    }
+
+
 }
