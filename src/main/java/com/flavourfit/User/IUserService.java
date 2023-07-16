1516aa41a39764e71800fdd2149a6db8cca5fbf1
Somya Agarwal <somyaagarwal@Somyas-MacBook-Pro.local>
2023-07-15 22:29:00 -0300

    Feature Gaf-70

--- src/main/java/com/flavourfit/User/IUserService.java
+++ src/main/java/com/flavourfit/User/IUserService.java
@@ -6,4 +6,8 @@ public interface IUserService {
     public String fetchAllUsers() throws SQLException;
 
     public int updateUser(UserDto user) throws SQLException;
+
+    public UserDto getUserbyID(int user) throws SQLException;
+
+    public PremiumUserDto getUserBymembership(int user) throws SQLException;
 }
--- src/main/java/com/flavourfit/User/UserService.java
+++ src/main/java/com/flavourfit/User/UserService.java
@@ -31,5 +31,14 @@ public class UserService implements IUserService {
         return this.userDao.updateUser(user);
     }
 
+    @Override
+    public UserDto getUserbyID(int user) throws SQLException{
+        return this.userDao.getUserById(user);
+    }
+
+    @Override
+    public PremiumUserDto getUserBymembership(int user) throws SQLException{
+        return this.userDao.getUserBymembership(user);
+    }
 
 }
