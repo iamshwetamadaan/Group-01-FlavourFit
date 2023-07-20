1979de5f446441c56c7e5dbbad83fcdb95792a3d
Dheeraj Bhat <dh210086@dal.ca>
2023-07-20 15:06:10 -0300

    gaf-48 get calorie history for graph

--- Backend/src/main/java/com/flavourfit/Trackers/TrackersController.java
+++ Backend/src/main/java/com/flavourfit/Trackers/TrackersController.java
@@ -1,7 +1,9 @@
 package com.flavourfit.Trackers;
 
 import com.flavourfit.Authentication.IAuthService;
+import com.flavourfit.Exceptions.CalorieHistoryException;
 import com.flavourfit.Helpers.DateHelpers;
+import com.flavourfit.ResponsesDTO.GetResponse;
 import com.flavourfit.ResponsesDTO.PutResponse;
 import com.flavourfit.Security.JwtService;
 import com.flavourfit.Trackers.Calories.*;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -72,7 +75,7 @@ public class TrackersController {
 
             logger.info("Updated record count for User #" + userId + ". Returning response through api");
             return ResponseEntity.ok().body(new PutResponse(true, "Successfully recorded calorie count", data));
-        } catch (SQLException e) {
+        } catch (CalorieHistoryException e) {
             logger.error("Bad api request during recordCalorieCount()");
             return ResponseEntity.badRequest().body(new PutResponse(false, "Failed to record calorieCount"));
         }
@@ -102,6 +105,24 @@ public class TrackersController {
         } catch (SQLException e) {
             logger.error("Bad api request during recordWaterIntake()");
             return ResponseEntity.badRequest().body(new PutResponse(false, "Failed to record Water intake"));
+        }
+    }
+
+    @GetMapping("/calorie-history")
+    public ResponseEntity<GetResponse> fetchCalorieHistory(
+            @RequestBody Map<String, Object> request,
+            @RequestHeader("Authorization") String token
+    ) {
+        logger.info("Entered controller method recordWaterIntake()");
+        String startDate = (String) request.get("startDate");
+        String endDate = (String) request.get("endDate");
+        int userId = authService.extractUserIdFromToken(token);
+
+        try {
+            List<CalorieGraphDto> calories = this.calorieHistoryService.fetchCalorieHistoryByPeriod(startDate, endDate, userId);
+            return ResponseEntity.ok().body(new GetResponse(true, "Successfully retrieved calorie history", calories));
+        } catch (CalorieHistoryException e) {
+            return ResponseEntity.badRequest().body(new GetResponse(false, "Failed to retrieved calorie history:" + e.getMessage()));
         }
     }
 }
