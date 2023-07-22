1f0759aaad84534018fbd09ae0fca721b9a5c525
rabiaa <rb618118@dal,ca>
2023-07-22 05:17:46 -0300

    Card Details DTO created

---
+++ Backend/src/main/java/com/flavourfit/User/PremiumUserCardDetailsDto.java
@@ -0,0 +1,60 @@
+package com.flavourfit.User;
+
+import java.sql.Date;
+
+public class PremiumUserCardDetailsDto {
+    private String cardNumber;
+    private String expiryMonth;
+    private String expiryYear;
+    private String cvv;
+    private Date startDate;
+    private Date endDate;
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getExpiryMonth() {
+        return expiryMonth;
+    }
+
+    public void setExpiryMonth(String expiryMonth) {
+        this.expiryMonth = expiryMonth;
+    }
+
+    public String getExpiryYear() {
+        return expiryYear;
+    }
+
+    public void setExpiryYear(String expiryYear) {
+        this.expiryYear = expiryYear;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+}
