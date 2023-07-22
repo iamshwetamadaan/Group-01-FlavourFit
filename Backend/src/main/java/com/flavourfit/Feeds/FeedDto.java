678a6d567665280ecae76bc41c0d14a99211f8dd
rabiaa <rb618118@dal,ca>
2023-07-22 16:11:06 -0300

    GAF79-Dto complete

--- Backend/src/main/java/com/flavourfit/Feeds/FeedDto.java
+++ Backend/src/main/java/com/flavourfit/Feeds/FeedDto.java
@@ -1,4 +1,40 @@
 package com.flavourfit.Feeds;
 
 public class FeedDto {
+    private int feedId;
+    private String feedContent;
+    private int likeCount;
+
+    public int getFeedId() {
+        return feedId;
+    }
+
+    public void setFeedId(int feedId) {
+        this.feedId = feedId;
+    }
+
+    public String getFeedContent() {
+        return feedContent;
+    }
+
+    public void setFeedContent(String feedContent) {
+        this.feedContent = feedContent;
+    }
+
+    public int getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(int likeCount) {
+        this.likeCount = likeCount;
+    }
+
+    @Override
+    public String toString() {
+        return "RecipeDto{" +
+                "feedId=" + feedId +
+                ", feedContent='" + feedContent + '\'' +
+                ", likes='" + likeCount + '\'' +
+                '}';
+    }
 }
