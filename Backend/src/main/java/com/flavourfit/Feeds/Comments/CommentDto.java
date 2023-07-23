fce34ad5d86cce7cfd737afd63663df90365c394
rabiaa <rb618118@dal,ca>
2023-07-22 21:54:38 -0300

    Comments DTO created for GAF-79

--- Backend/src/main/java/com/flavourfit/Feeds/Comments/CommentDto.java
+++ Backend/src/main/java/com/flavourfit/Feeds/Comments/CommentDto.java
@@ -1,4 +1,31 @@
 package com.flavourfit.Feeds.Comments;
 
 public class CommentDto {
+    private int commentID;
+    private String commentContent;
+    private String username;
+
+    public int getCommentID() {
+        return commentID;
+    }
+
+    public void setCommentID(int commentID) {
+        this.commentID = commentID;
+    }
+
+    public String getCommentContent() {
+        return commentContent;
+    }
+
+    public void setCommentContent(String commentContent) {
+        this.commentContent = commentContent;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
