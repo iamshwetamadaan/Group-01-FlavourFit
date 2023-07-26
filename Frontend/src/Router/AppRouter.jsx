40c68a3339d2fdc858fc29e7753941f3d6492038
bharathsr.0912@gmail.com <bh626198@dal.ca>
2023-07-26 16:27:33 -0300

    User Profile UI

--- Frontend/src/Router/AppRouter.jsx
+++ Frontend/src/Router/AppRouter.jsx
@@ -7,16 +7,20 @@ import Login from "../Login/Login";
 import Home from "../Home/Home";
 import Registration from "../Registration/Registration";
 import GuestLogin from "../GuestLogin/GuestLogin";
+import UserProfile from "../UserProfile/userProfile";
 
 const AppRouter = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="home" element={<Home />} />
+      
       </Route>
       <Route path="/login" element={<Login />} />
       <Route path="/guest-login" element={<GuestLogin />} />
       <Route path="/register" element={<Registration />} />
+      <Route path="/edit-profile" element={<UserProfile/>}/>
+     
     </Routes>
   </BrowserRouter>
 );
